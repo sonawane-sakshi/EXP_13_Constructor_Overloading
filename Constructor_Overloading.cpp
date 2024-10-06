ea141e754c87cab6d270a2c494ee514fa6b756d5
sonawane-sakshi <h116sonawanesakshi@gmail.com>
2024-10-06 18:20:56 +0530

    Add files via upload

---
+++ Constructor_Overloading.cpp
@@ -0,0 +1,27 @@
+//Sakshi Sonawane
+//PRN:24070123505
+#include <iostream>
+using namespace std;
+class Addition
+{
+   public:
+   int sum(int a, int b)
+   {
+    return a+b;
+   } 
+   int sum(int a,int b, int c)
+   {
+    return a+b+c;
+   }
+   float sum(int a, float b)
+   {
+    return a+b;
+   }
+} ;
+int main(void)
+{
+    Addition obj;
+    cout<<obj.sum(20,15)<<endl;
+    cout<<obj.sum(12,33,45)<<endl;
+    cout<<obj.sum(132,23.44f)<<endl;
+}
